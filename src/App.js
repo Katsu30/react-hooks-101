d4a41819fe7d008dbdb156b7d09e4502a934a550
Katsuhiro <sc30.katsu@gmail.com>
2020-05-18 10:41:12 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  // useStateの引数は、配列で返されるので分割代入で「状態名」と「動作名」で分ける
+  // 慣習的に第二引数は、set~という名前にする
+  const [count, setCount] = useState(0)
+
+  // stateの変更を行いたい場合は、第二引数で選択した関数名を使用する
+  const increment = () => {
+    setCount(count + 1);
+  }
+  const decrement = () => {
+    setCount(count - 1);
+  }
+
+  // 前の値を引数に取り、処理を加えることもできる
+  const increment2 = () => {
+    setCount((previousCount) => previousCount + 1);
+  }
+  const decrement2 = () => {
+    setCount((previousCount) => previousCount -1);
+  }
+
+  // 直接、値を代入するときは特別なプロパティ名などは必要ない
+  const reset = () => {
+    setCount(0);
+  }
+  const multiple = () => {
+    setCount(count * 2);
+  }
+  const divided3 = () => {
+    if(count % 3 === 0){
+      setCount(count / 3);
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>count: {count}</div>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+      <button onClick={increment2}>+1</button>
+      <button onClick={decrement2}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <button onClick={multiple}>x2</button>
+      <button onClick={divided3}>3の倍数のときのみ３で割る</button>
     </div>
   );
 }
