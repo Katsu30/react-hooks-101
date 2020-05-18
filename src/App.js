58316c08f0e8ea4338724d98df98b295ebb310d0
Katsuhiro <sc30.katsu@gmail.com>
2020-05-18 10:57:10 +0900

    multiple state

--- src/App.js
+++ src/App.js
@@ -1,52 +1,31 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  // useStateの引数は、配列で返されるので分割代入で「状態名」と「動作名」で分ける
-  // 慣習的に第二引数は、set~という名前にする
-  const [count, setCount] = useState(0)
+const App = (props) => {
+  // 状態の初期化
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
 
-  // stateの変更を行いたい場合は、第二引数で選択した関数名を使用する
-  const increment = () => {
-    setCount(count + 1);
-  }
-  const decrement = () => {
-    setCount(count - 1);
-  }
-
-  // 前の値を引数に取り、処理を加えることもできる
-  const increment2 = () => {
-    setCount((previousCount) => previousCount + 1);
-  }
-  const decrement2 = () => {
-    setCount((previousCount) => previousCount -1);
-  }
-
-  // 直接、値を代入するときは特別なプロパティ名などは必要ない
   const reset = () => {
-    setCount(0);
+    setPrice(props.price);
+    setName(props.name);
   }
-  const multiple = () => {
-    setCount(count * 2);
-  }
-  const divided3 = () => {
-    if(count % 3 === 0){
-      setCount(count / 3);
-    }
-  }
-
 
   return (
-    <div>
-      <div>count: {count}</div>
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>-1</button>
-      <button onClick={increment2}>+1</button>
-      <button onClick={decrement2}>-1</button>
-      <button onClick={reset}>Reset</button>
-      <button onClick={multiple}>x2</button>
-      <button onClick={divided3}>3の倍数のときのみ３で割る</button>
-    </div>
-  );
+      <div>
+        <p>現在の{name}は、{price}円です</p>
+        <button onClick={() => setPrice(price + 1)}>+1</button>
+        <button onClick={() => setPrice(price - 1)}>-1</button>
+        <button onClick={reset}>Reset</button>
+        <input value={name} onChange={e => setName(e.target.value)}/>
+      </div>
+    );
 }
+
+// propsを利用する方法もある
+App.defaultProps =  {
+  name: '',
+  price: 1000
+}
+
 
 export default App;
