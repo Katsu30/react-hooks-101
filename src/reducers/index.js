45f9e847174d0e2e5aa8064f3dc18601f9d1e9ce
Katsuhiro <sc30.katsu@gmail.com>
2020-05-20 20:32:08 +0900

    implement logs

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,31 +1,9 @@
-import {
-  CREATE_EVENT,
-  DELETE_EVENT,
-  DELETE_ALL_EVENTS
-} from '../actions';
+import { combineReducers } from 'redux';
 
-// action {
-//   type: 'CREATE_EVENT'
-//   title: '2020東京オリンピックのお知らせ'
-//   body: '2020年に東京でオリンピックを開催します'
-//}
+import events from './events';
+import operationLogs from './operationLogs';
 
-// reducerを作成し、実際の処理をまとめる
-// reduxと同じように、typeによって場合わけをする
-const events = (state = [], action) => {
-  switch (action.type) {
-    case CREATE_EVENT:
-      const event = { title: action.title, body: action.body }
-      const length = state.length
-      let id = length === 0 ? 1 : state[length -1].id +1
-      return [...state, { id: id, ...event }]
-    case DELETE_EVENT:
-      return state.filter(event => event.id !== action.id);
-    case DELETE_ALL_EVENTS:
-      return []
-    default:
-      return state
-  }
-}
-
-export default events;
+export default combineReducers({
+  events,
+  operationLogs
+})
