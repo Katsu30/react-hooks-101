7cb37c287e906bcc54e9e7e8738250d3390f1503
Katsuhiro <sc30.katsu@gmail.com>
2020-05-20 12:48:54 +0900

    use useContext and refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React, { useReducer } from 'react';
 // bootstrapのインストールは下記
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reducer from '../reducers'
+import AppContext from '../contexts/AppContext';
+
 import EventForm from '../components/EventForm';
 import Events from '../components/Events';
 
@@ -10,11 +12,17 @@ const App = (props) => {
   // 処理を実行したいタイミングでdispatchを呼ぶ
   const [state, dispatch] = useReducer(reducer, []);
 
+  // Providerの使用方法
+  // Valueに設定した値を子コンポーネントでも使用できる
+  // このvalueはuseContextを使用することで、propsを経由しないでデータを取得できる
+  // Providerのvalueには、オブジェクトも設定できるので、stateやdispatchを設定することで配下のどこでも使用が可能になる
   return (
-      <div className="container-fluid">
-        <EventForm state={state} dispatch={dispatch}/>
-        <Events state={state} dispatch={dispatch}/>
-      </div>
+      <AppContext.Provider value={{ state, dispatch }}>
+        <div className="container-fluid">
+          <EventForm />
+          <Events />
+        </div>
+      </AppContext.Provider>
     );
 }
 
